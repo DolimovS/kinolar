216d23f2706f51fe82adb0f148f3e7c85d904d76
DolimovS <samandardolimov71@gmail.com>
2025-11-28 20:45:26 +0500

    user is corusel add

--- pages/Admin/dashboard/Dashboard.jsx
+++ pages/Admin/dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 
-import SearchInput  from "../../../src/components/searchInput/SearchInput"
+import SearchInput from "../../../src/components/searchInput/SearchInput"
 import "./Dashboard.css"
 import { useDashboardInputStore } from "../../../src/store/useAppStore"
 import { useEffect, useState } from "react"
@@ -7,29 +7,29 @@ import api from "../../../src/utils/axios"
 import Cards from "../../../src/components/Cards"
 
 const Dashboard = () => {
-  const searchInput=useDashboardInputStore((state)=>state.text)
-  const [reklamaDashboardMovie,setReklamaDashboardMovie]=useState([])
-  const [searchDashboardMovie,setSearchDashboardMovie]=useState([])
-// malumot olish
-  const fetchMoviesDashboard= async()=>{
-    const res=await api.get("/movies")
-    const movie_beshta=res.data.reverse().slice(0,5)
+  const searchInput = useDashboardInputStore((state) => state.text)
+  const [reklamaDashboardMovie, setReklamaDashboardMovie] = useState([])
+  const [searchDashboardMovie, setSearchDashboardMovie] = useState([])
+  // malumot olish
+  const fetchMoviesDashboard = async () => {
+    const res = await api.get("/movies")
+    const movie_beshta = res.data.reverse().slice(0, 5)
     setReklamaDashboardMovie(movie_beshta)
     setSearchDashboardMovie(res.data)
   }
   console.log(reklamaDashboardMovie);
-  useEffect(()=>{
+  useEffect(() => {
     fetchMoviesDashboard()
-  },[])
+  }, [])
 
   // filtered
-  const   filterMovies=searchDashboardMovie.filter((movie=>{
+  const filterMovies = searchDashboardMovie.filter((movie => {
     return movie.movie_name.toLowerCase().includes(searchInput.toLowerCase())
   }))
   console.log(searchDashboardMovie);
-  
+
   console.log(filterMovies);
-  
+
 
 
 
@@ -39,20 +39,33 @@ const Dashboard = () => {
         <h1>Dashboard</h1>
       </div>
       <div className="dashboard_input">
-      <SearchInput />
+        <SearchInput />
       </div>
-      {searchInput}
+      {/* {searchInput} */}
+        
+        <div className="input_dashboard_results">
+
+
+          {searchInput.length > 0 && filterMovies.length === 0 && <p className="no_found_dashboard">"{searchInput}" nomli kino topilmadi :(</p>}
+      
+        
+      <div className="search_input_result">
+        {searchInput.length > 0 && filterMovies.length > 0 && filterMovies?.map((kino) => (
+          <Cards key={kino._id} id={kino._id} KinoNomi={kino.movie_name} KinoJanr={kino.janr} KinoImg={kino.imgUrl} />
+        ))}
+      </div>
+        </div>
 
 
       <div className="movies_dashboard">
-        <div className="movie_sarlavha">
-          <h2 className="">Oxirgi filmlar</h2>
+        <div className="movie_salavha">
+          <h2 className="">So'nggi filmlar</h2>
         </div>
 
         <div className="dashboard_slader">
           {reklamaDashboardMovie?.map((kino) => (
-            <Cards  key={kino._id} id={kino._id} KinoNomi={kino.movie_name}  KinoJanr={kino.janr} KinoImg={kino.imgUrl} />
-        ))}
+            <Cards key={kino._id} id={kino._id} KinoNomi={kino.movie_name} KinoJanr={kino.janr} KinoImg={kino.imgUrl} />
+          ))}
         </div>
         {/* <Posters kinoHaqidaMalumotlar={reklamaDashboardMovie} /> */}
       </div>
--- pages/User/kinolar/kinolar.jsx
+++ pages/User/kinolar/kinolar.jsx
@@ -4,7 +4,7 @@ import Corusel from "../../../src/components/corusel/Corusel"
 const kinolar = () => {
   return (
     <div className="kinolar_container">
-
+      
       <Corusel />
       {/* <div className="konolar box"></div>
       <div className="konolar box"></div>
